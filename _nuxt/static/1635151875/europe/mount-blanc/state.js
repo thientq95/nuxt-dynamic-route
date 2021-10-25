b1bdf052da86ff319118ef1c5c1945874b861c3f
tqthien <tqthien@ascvn.edu.vn>
2021-10-25 16:17:45 +0700

    0b5cbe0

---
+++ _nuxt/static/1635151875/europe/mount-blanc/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-dynamic-route\u002F_nuxt\u002Fstatic\u002F1635151875",layout:"default",error:a,serverRendered:true,routePath:"\u002Feurope\u002Fmount-blanc",config:{_app:{basePath:"\u002Fnuxt-dynamic-route\u002F",assetsPath:"\u002Fnuxt-dynamic-route\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1635151875/rivers/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/rivers", {data:[{slug:"rivers"}],fetch:{},mutations:void 0});
