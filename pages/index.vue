aeb8c7e2d3b6c1e60b38702778a2f4b4854f9dcf
tqthien <tqthien@ascvn.edu.vn>
2021-10-25 15:48:29 +0700

    init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul v-for="mountain in mountains" :key="mountain.id">
+      <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
+        <li>{{ mountain.title }}</li>
+      </NuxtLink>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData() {
+    const mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+
+    return { mountains }
+  }
+}
+</script>
